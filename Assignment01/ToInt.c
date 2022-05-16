16122a2d133316d82cbb7b842a2f09202726f62f
Narapat Patcharapornpun <narapat@hotmail.com>
2022-05-17 00:01:41 +0700

    finish assignment 1,2,3

--- Assignment01/ToInt.c
+++ Assignment01/ToInt.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 #include<string.h>
-
 
 /*
 Task 2: Write a program that read a string
@@ -12,30 +11,30 @@ Return an array of Integer
 Example: [1, 2, 12, 20, 50, 8, 4]
 Save the file as toInt.c
 */
-int toInt(char ListOfNumber[],int ArrayOfNumber[]){
-    int CountOfNumber=0;
 
-    printf("get string list : %s\n", ListOfNumber);
-    char* token = strtok(ListOfNumber, ",");
+int toInt(char ListOfNumber[],int ArrayOfNumber[]){
+    int iCountOfNumber=0;
     int i=0;
+    printf("get string list : %s\n", ListOfNumber);
+    char *token = strtok(ListOfNumber, ",");
+    
     while (token != NULL) {
         ArrayOfNumber[i] = atoi(token);
-        //printf("%d\n", ArrayOfNumber[i]);
         token = strtok(NULL,",");
         i++;
     }
-    CountOfNumber = i; 
-return CountOfNumber;
+    iCountOfNumber = i; 
+return iCountOfNumber;
 }
 
 int main()
 { 
-    char ListOfNumber[] = "3, 1, 2, 12, 20, 50, 8, 4";
+    char ListOfNumber[] = "1, 2, 12, 20, 50, 8, 4";
     int ArrayOfNumber[100];
     int number = toInt(ListOfNumber, ArrayOfNumber);
     printf("Dispaly Array of Integer\n");
     for(int i=0;i<number;i++){
-        printf("position %d: %d\n", i,ArrayOfNumber[i]);
+        printf("position %d: %d\n", i, ArrayOfNumber[i]);
     }
 return 0;
 }
--- Assignment01/average.c
+++ Assignment01/average.c
@@ -1,4 +1,7 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 /*
 Task 3: From Task 2 of assignment 1
 
@@ -7,3 +10,45 @@ For example, input is [1, 2, 12, 20, 50, 8, 4]
 Average is 13.85714
 Save the file as average.c
 */
+
+int toInt(char ListOfNumber[],int ArrayOfNumber[]){
+    int iCountOfNumber=0;
+    int i=0;
+    printf("get string list : %s\n", ListOfNumber);
+    char *token = strtok(ListOfNumber, ",");
+    
+    while (token != NULL) {
+        ArrayOfNumber[i] = atoi(token);
+        token = strtok(NULL,",");
+        i++;
+    }
+    iCountOfNumber = i; 
+return iCountOfNumber;
+}
+
+float arrayAVG(int ArrayOfNumber[], int SizeOfArray){
+    float fAverage = 0.0;
+    for(int i=0;i<SizeOfArray;i++){
+        fAverage += ArrayOfNumber[i];
+    }
+    fAverage = fAverage/SizeOfArray;
+    
+return fAverage;
+}
+
+int main()
+{ 
+    char ListOfNumber[] = "1, 2, 12, 20, 50, 8, 4";
+    int ArrayOfNumber[100];
+    int Number = toInt(ListOfNumber, ArrayOfNumber);
+    
+    printf("Dispaly Array of Integer\n");
+    for(int i=0;i<Number;i++){
+        printf("position %d: %d\n", i, ArrayOfNumber[i]);
+    }
+
+    float Average = arrayAVG(ArrayOfNumber,Number);
+    printf("Average of the list :%f",Average);
+
+return 0;
+}
