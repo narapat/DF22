8424c18f087e14414603b658eb0e8157b8146d6d
Narapat Patcharapornpun <narapat@hotmail.com>
2022-05-16 23:30:18 +0700

    commit toInt.c

--- Assignment01/ToInt.c
+++ Assignment01/ToInt.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include<string.h>
 
 
 /*
@@ -11,39 +12,30 @@ Return an array of Integer
 Example: [1, 2, 12, 20, 50, 8, 4]
 Save the file as toInt.c
 */
+int toInt(char ListOfNumber[],int ArrayOfNumber[]){
+    int CountOfNumber=0;
 
-void main()
-{
-    char ListOfNumber[] = "1,2,12,20,50,8,4";
-    int ArrayOfInteger[30];
-    int Asc;
-    char tmp[3];
-    //printf("Please enter list of number (with comma): ");
-    //scanf("%s", ListOfNumber);
-    int i = 0;
-    int j = 0;
-    printf("%c\n",ListOfNumber);
-
-    do {
-        //tmp="";
-        int k = 0;
-        if(ListOfNumber[i]!=44){
-            printf("%c",ListOfNumber[i]);
-            tmp[k]=ListOfNumber[i];
-            i++;
-            k++;
-        }else{
-            printf("%c\n",ListOfNumber[i]);
-        }
-        //Asc = (int)tmp;
-        //ArrayOfInteger[j]=Asc;
-        j++;
+    printf("get string list : %s\n", ListOfNumber);
+    char* token = strtok(ListOfNumber, ",");
+    int i=0;
+    while (token != NULL) {
+        ArrayOfNumber[i] = atoi(token);
+        printf("%d\n", ArrayOfNumber[i]);
+        token = strtok(NULL,",");
         i++;
     }
-    while (i<sizeof(ListOfNumber));
-/*
-    for(int i=0;i<30;i++){
-        printf("%d\n",ArrayOfInteger[i]);
+    CountOfNumber = i; 
+return CountOfNumber;
+}
+
+int main()
+{
+    char ListOfNumber[] = "1,2,12,20,50,8,4";
+    int ArrayOfNumber[100];
+    int number = toInt(ListOfNumber, ArrayOfNumber);
+    printf("Dispaly Array of Integer\n");
+    for(int i=0;i<number;i++){
+        printf("%d:%d\n", i,ArrayOfNumber[i]);
     }
-*/
+return 0;
 }
